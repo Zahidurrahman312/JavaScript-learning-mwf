d601d47c96183743a529f28e2418b6e6736223f6
unknown <zahidrehamn886554@gmail.com>
2025-10-01 02:38:15 +0500

    math method done

--- Class 18/app.js
+++ Class 18/app.js
@@ -6,6 +6,56 @@ console.log('javascript connected!');
 
 // console.log(Math.floor(Math.random()*9000)+50000);
 
-console.log(Math.floor(Math.random()*(12-3+5))+5);
+// console.log(Math.floor(Math.random()*(12-3+5))+5);
+
+// we create a game through random number
+
+// var game = Math.floor(Math.random()*4);
+
+// var user = +prompt("enter a number ");
+
+// if(user === game){
+
+//     alert("success ! ");
+
+// }else{
+//     alert("try again");
+// }
+
+// converting string into number ====> integer and decimal
+
+// var num = "23";
+
+// console.log(parseInt(num));
+
+// string ko decimal ky sath convert krny kaily alg method hai
+// var num = "24.45";
+
+// console.log( parseFloat(num));
+
+// var num = "345";
+
+// console.log(Number(num));
+
+// //es ka short form 
+
+// console.log(+num);
+
+//converting number into string
+
+// var age = 24;
+
+// console.log(age.toString());
+// //es ka ek or tarika ye bhi hai
+
+// console.log(String(age));
+
+// ager ham ko decimal ky bad apni marzi ky number chai thu ye wala tarika
+
+// var price = 23.904503805899;
+
+// console.log(price.toFixed(2));
+// console.log(price.toFixed(3));
+
 
 
---
+++ Class 19/app.js
@@ -0,0 +1,104 @@
+console.log('Javascript connected !');
+
+// var now = new Date();
+
+// console.log(now);
+
+// var now = new Date();
+
+// var newDay = mon.getDay();
+
+// console.log(newDay);
+
+// var now = new Date();
+// var month = now.getMonth();
+
+// console.log(month);
+
+// var weekDay = now. getHours();
+// console.log(weekDay);
+
+// var weekDay = ["sun","sun","Tue","wed","Thu","fri","sat"];
+
+// var now = new Date();
+
+// var weekFull = now.getDay();
+
+// var weekdays = weekDay[weekFull];
+
+// console.log(weekdays);
+
+// var now = new Date();
+
+// var second = now.getMinutes();
+
+// console.log(second);
+// var now = new Date();
+// var  minute = now.getDay(6);;
+
+// console.log(minute);
+
+// var month =["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"];
+
+// var days= ["sun","mon","Tue","wed","Thr","fri"];
+
+
+
+// var now = new Date();
+
+// var monthFound = now.getMonth();
+
+// var daysFound = now.getDay();
+
+
+// var ofMonth = month [monthFound];
+
+// var ofDays = days [daysFound];
+
+// var yearFull = now.getFullYear();
+
+// console.log("Today is a " + ofDays + " and month is "+monthFound +" "+ ofMonth +" "+yearFull);
+
+// console.log("Today is " +ofDays +" "+ yearFull);
+
+// var secon = new Date();
+
+// var secon1 = secon.getSeconds();
+
+// console.log(secon1);
+
+// var milmi = secon.getMilliseconds();
+
+// console.log(milmi);
+
+// for(var i = 1; i < 5; i++){
+
+//     for(var j = 1 ; j < 5 ; j++){
+
+//         document.write(" *");
+//     }
+
+//     document.write("<br />");
+// }
+
+
+// for( var i = 1; i <= 5; i++){
+
+//     for(var j = 1; j <=i; j++){
+
+//         document.write( j );
+//     }
+//     document.write(" <br />");
+// }
+
+// for(var i =1; i <=2; i++){
+
+//     for(var j =1; j <=4; j++){
+
+//         document.write(" * ");
+//     }
+//     document.write(" <br />");
+// }
+
+
+
