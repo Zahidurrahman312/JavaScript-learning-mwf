a0751275e15ce2cdaa0cd1e636d2a5ef3d89ed25
unknown <zahidrehamn886554@gmail.com>
2025-10-14 00:15:32 +0500

    revesion

---
+++ Class 23/app.js
@@ -0,0 +1,40 @@
+console.log("Javascript connected!");
+
+//revision of function
+
+// var abc = "zahid";
+
+// function add(name ="producer",age =20){
+// console.log("Hi, I am " +name + " "+age );
+
+// }
+// add(abc,22);
+
+// var user =prompt('enter your name ');
+
+// var user2 = prompt('enter age ');
+
+// function foo(){
+
+//     console.log("Hi I am " + user + " "+ user2);
+    
+// }
+// foo();
+//parameter kk ander ham default value store karsakth hai
+// function fullname(name= "producer",){
+
+//     console.log("Hi, my name is " +name );
+    
+// }
+// fullname()
+
+function addnumber(){
+
+    
+    return  ;
+
+}
+addnumber();
+var result = addnumber(3,5);
+console.log(result);
+
---
+++ Class 24/app.js
@@ -0,0 +1,36 @@
+console.log("JavaScript connected!");
+
+// var num1 = Math.random();
+
+// console.log(num1);
+// ager ham ko specific number chai hu kasy karangy 
+
+// var num = Math.floor(Math.random() *4000 +1000);
+
+// console.log(num);
+// console.log(Math.floor(Math.random()*(15-5+1)));
+
+// var num = Math.floor(Math.random()*1001+1000);
+
+// console.log(num);
+
+// var xyz = "zahidurrahman";
+
+// console.log(xyz);
+
+// var newstr = xyz.charAt(4)
+// console.log(newstr);
+
+// var newname = "zahidurahman";
+
+// var str = newname.split(",")
+
+// console.log(str);
+
+//  chart at ()  ====> function hamisha number lathy hai or string retutn karthy hai
+
+// inedxof ()  ====> hamisha string lathy hai or number return kartha hai
+
+
+
+
