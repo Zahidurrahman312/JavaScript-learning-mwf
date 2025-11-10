568fe30184a4358abba9644a289c595829d7b827
unknown <zahidrehamn886554@gmail.com>
2025-11-11 00:40:27 +0500

    image sawape onmouse over

---
+++ Class 27/app.js
@@ -0,0 +1,18 @@
+console.log("javascript connected!");
+
+// var para = document.getElementById("para1");
+
+// console.log(para);
+
+// var paras = para.parentNode.parentNode;
+
+// console.log(paras);
+
+var ele = document.getElementsByTagName("p");
+
+console.log(ele);
+
+
+
+
+
---
+++ Class 28/app.js
@@ -0,0 +1,12 @@
+console.log("Javascript connected!");
+
+// function foo(name){
+//     alert("Hello i am there," + name)
+// }
+
+// This is a onclick event
+
+
+
+
+
