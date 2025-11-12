3ad15b92913e8ec770118a9eb7b7af07f61877bc
unknown <zahidrehamn886554@gmail.com>
2025-11-13 03:09:42 +0500

    nodeName and attribute

---
+++ Class 29/app.js
@@ -0,0 +1,33 @@
+console.log("JavaScript connected!");
+
+// var link = document.getElementById("p1");
+
+// console.log(link.hasAttribute("href"));  // hasAttribute =====> es ky through ham dak sakthy hai kk  aya es
+// console.log(link.hasAttribute("class")); // kk pas consa attribute hai ham ko return kartha hai ture or false
+
+// console.log(link.getAttribute("href")); // getAttribute ====> es kk through ham wo attribute access karsakthy
+// console.log(link.getAttribute("class")); // hai ager hu thu daga ager nahi thu null show hoga
+
+// // ab ha attribute  set kasy karth hai ham sy value lathy ek consa attribute second name
+
+//  var links =link.setAttribute("class","amazon");
+
+// console.log(link);
+
+// var list = document.getElementById("p1").attributes.length;
+
+
+// var attrib = list[2].nodeName;
+
+// console.log(attrib);
+
+
+// console.log(list);
+
+
+
+
+
+
+
+
